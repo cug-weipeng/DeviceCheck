d229881eba77f8b7084de4cf198d7b32126f2ab6
wei peng <cug_weipeng@163.com>
2017-11-17 14:19:00 +0800

    1

--- SoundShow/BaseShow.cs
+++ SoundShow/BaseShow.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace SoundShow
 {
+    interface ICharge
+    {
+        void charge();
+    }
     public abstract class BaseShow
     {
+        public event Action FireHandlerEvent;
         public string Actor { get; set; }
         public string Desktop { get; set; }
         public string Chair { get; set; }
@@ -18,25 +23,28 @@ namespace SoundShow
         public string Ruler { get; set; }
         public void Start()
         {
-            Console.Write("表演开始了");
+            Console.WriteLine("表演开始了");
         }
         public abstract void DogBark();
         public abstract void PeopelTalk();
         public abstract void WindBlow();
         public virtual void OpenRemark()
         {
-            Console.Write($"{this.GetType()}:欢迎收听口技表演！");
+            Console.WriteLine($"{this.GetType()}:欢迎收听口技表演！");
         }
         public virtual void EndRemark()
         {
-            Console.Write($"{this.GetType()}:谢谢大家的收听！");
+            Console.WriteLine($"{this.GetType()}:谢谢大家的收听！");
         }
     }
     /// <summary>
     /// 南派口技表演，绝活变魔术，魔术毯，有个小助理
     /// </summary>
-    public class SouthShow:BaseShow
+    public class SouthShow:BaseShow,ICharge
     {
+        /// <summary>
+        /// 助理
+        /// </summary>
         public string Assistant { get; set; }
         /// <summary>
         /// 魔术毯
@@ -44,7 +52,148 @@ namespace SoundShow
         public string MagicCarpet;
         public void MagicShow()
         {
-            Console.Write($"{this.GetType()}:用{this.MagicCarpet}遮住{this.Assistant}，拉开{this.MagicCarpet}，{this.Assistant}变成了小兔子。");
+            Console.WriteLine($"{this.GetType()}:用{this.MagicCarpet}遮住{this.Assistant}，拉开{this.MagicCarpet}，{this.Assistant}变成了小兔子。");
         }
+        public override  void DogBark()
+        {
+            Console.WriteLine($"{this.GetType()}:汪汪汪，汪汪汪，汪汪汪汪汪汪汪");
+        }
+        public override void PeopelTalk()
+        {
+            Console.WriteLine($"{this.GetType()}:妇人惊觉欠伸，其夫呓语。既而儿醒，大啼。夫亦醒。妇抚儿乳，儿含乳啼，妇拍而呜之");
+        }
+        public override void WindBlow()
+        {
+            Console.WriteLine($"{this.GetType()}: 呼呼~~~~~~");
+
+        }
+        public override void OpenRemark()
+        {
+            Console.WriteLine($"{this.GetType()}:好高兴见到大家，希望大家钟意我的表演！");
+        }
+        public void charge()
+        {
+            Console.WriteLine($"{this.GetType()}: 一块两块也是爱，十块八块的都很帅");
+        }
+    }
+    /// <summary>
+    /// 北派口技表演，绝活二人转，手帕，有个小徒弟
+    /// </summary>
+    public class NorthShow : BaseShow,ICharge
+    {
+        /// <summary>
+        /// 徒弟
+        /// </summary>
+        public string Apprentice { get; set; }
+        /// <summary>
+        /// 手帕
+        /// </summary>
+        public string Handkerchief;
+        public void ErRenZhuan()
+        {
+            Console.WriteLine($"{this.GetType()}:{this.Actor}：“挖个坑、埋点土、数个一二三四五”。{this.Apprentice}：“自己的土自己的地、种啥都长人民币”，转起{this.Handkerchief}");
+        }
+        public override void DogBark()
+        {
+            Console.WriteLine($"{this.GetType()}:汪，汪汪，汪，汪汪汪汪汪汪汪");
+        }
+        public override void PeopelTalk()
+        {
+            Console.WriteLine($"{this.GetType()}:妇人惊觉欠伸，其夫呓语。既而儿醒，大啼。夫亦醒。妇抚儿乳，儿含乳啼，妇拍而呜之");
+        }
+        public override void WindBlow()
+        {
+            Console.WriteLine($"{this.GetType()}: 嗖嗖~~~~~~");
+
+        }
+        public override void EndRemark()
+        {
+            Console.WriteLine($"{this.GetType()}:谢谢各位老铁的掌声！");
+        }
+
+        public void charge()
+        {
+            Console.WriteLine($"{this.GetType()}: 有钱的捧个钱场，没钱的捧个人场！");
+        }
+    }
+    /// <summary>
+    /// 西派口技表演，绝活唱山歌，头巾，有个搭档
+    /// </summary>
+    public class WestShow : BaseShow,ICharge
+    {
+        /// <summary>
+        /// 搭档
+        /// </summary>
+        public string Partner { get; set; }
+        /// <summary>
+        /// 头巾
+        /// </summary>
+        public string Headkerchief;
+        public void SingMountSong()
+        {
+            Console.WriteLine($"{this.GetType()}:{this.Actor}裹着{this.Headkerchief}唱到：“山丹丹的那个开花呦”。{this.Partner}：“红艳艳~~~,红艳艳~~~”");
+        }
+        public override void DogBark()
+        {
+            Console.WriteLine($"{this.GetType()}:汪汪汪汪汪汪汪");
+        }
+        public override void PeopelTalk()
+        {
+            Console.WriteLine($"{this.GetType()}:妇人惊觉欠伸，其夫呓语。既而儿醒，大啼。夫亦醒。妇抚儿乳，儿含乳啼，妇拍而呜之");
+        }
+        public override void WindBlow()
+        {
+            Console.WriteLine($"{this.GetType()}: 哗哗~~~~~~");
+
+        }
+        public override void EndRemark()
+        {
+            Console.WriteLine($"{this.GetType()}:额的表演就是这，希望大家伙儿喜欢！");
+        }
+        public override void OpenRemark()
+        {
+            Console.WriteLine($"{this.GetType()}:能在这里给大伙表演，额高兴滴很！");
+        }
+
+        public void charge()
+        {
+            Console.WriteLine($"{this.GetType()}:在家靠父母，出门靠朋友，走南闯北不容易，大伙多鼓励");
+        }
+    }
+    /// <summary>
+    /// 东派口技表演，绝活脱口秀，西装，有个秘书
+    /// </summary>
+    public class EastShow : BaseShow,ICharge
+    {
+        /// <summary>
+        /// 秘书
+        /// </summary>
+        public string Secretary { get; set; }
+        /// <summary>
+        /// 西装
+        /// </summary>
+        public string suit;
+        public void TalkShow()
+        {
+            Console.WriteLine($"{this.GetType()}:{this.Actor}穿着{this.suit}说到：“说金钱是罪恶，都在捞；说美女是祸水，都想要；说高处不胜寒，都在爬；说烟酒伤身体，都不戒；说天堂最美好，都不去！ ”");
+        }
+        public override void DogBark()
+        {
+            Console.WriteLine($"{this.GetType()}:汪汪，嗷呜~~~~~");
+        }
+        public override void PeopelTalk()
+        {
+            Console.WriteLine($"{this.GetType()}:妇人惊觉欠伸，其夫呓语。既而儿醒，大啼。夫亦醒。妇抚儿乳，儿含乳啼，妇拍而呜之");
+        }
+        public override void WindBlow()
+        {
+            Console.WriteLine($"{this.GetType()}: 沙沙~~~~~~");
+
+        }
+        public void charge()
+        {
+            Console.WriteLine($"{this.GetType()}:一块两块，你买不了烟，也买不了酒，却能买个好心情，请大家多多支持！");
+        }
+
     }
 }
--- SoundShow/Program.cs
+++ SoundShow/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,143 @@ namespace SoundShow
 {
     class Program
     {
+        static string  spliter = "****************************************************************************";
+        static string shortSpliter = "*****************************";
         static void Main(string[] args)
         {
+            BaseShow baseShow;
+            NorthShow northShow = new NorthShow()
+            {
+                Actor = "赵大山",
+                Desktop = "八仙桌",
+                Chair = "太师椅",
+                Ruler = "扶尺",
+                Fan = "折扇",
+                Apprentice = "沈小阳",
+                Handkerchief = "红手帕"
+            };
+            SouthShow southShow = new SouthShow()
+            {
+                Actor = "刘大谦",
+                Desktop = "魔术桌",
+                Chair = "魔术椅",
+                Ruler = "扶尺",
+                Fan = "折扇",
+                Assistant = "董小卿",
+                MagicCarpet = "黑色魔毯"
+            };
+            EastShow eastShow = new EastShow()
+            {
+                Actor = "周大波",
+                Desktop = "办工桌",
+                Chair = "办公椅",
+                Ruler = "扶尺",
+                Fan = "折扇",
+                suit = "黑西服",
+                Secretary = "美女秘书"
+            };
+            WestShow westShow = new WestShow()
+            {
+                Actor = "阿大宝",
+                Desktop = "课桌",
+                Chair = "板凳",
+                Ruler = "扶尺",
+                Fan = "折扇",
+                Partner = "王小妮",
+                Headkerchief = "白头巾"
+            };
+
+            Console.WriteLine("自我介绍");
+            ShowDetail(northShow);
+            ShowDetail(southShow);
+            ShowDetail(eastShow);
+            ShowDetail(westShow);
+            Console.WriteLine(spliter);
+            Console.WriteLine("");
+
+            Console.WriteLine("开始表演");
+            play(southShow);
+            play(northShow);
+            play(westShow);
+            play(eastShow);
+            Console.WriteLine(spliter);
+            Console.WriteLine("");
+            
+            //绝活表演
+            JueHuo(southShow.MagicShow);
+            JueHuo(northShow.ErRenZhuan);
+            JueHuo(westShow.SingMountSong);
+            JueHuo(eastShow.TalkShow);
+            Console.WriteLine(spliter);
+            Console.WriteLine("");
+
+            //火起事件
+            southShow.FireHandlerEvent += () => { Console.WriteLine("夫起大户：快起来呀，着火了！"); };
+            southShow.FireHandlerEvent += () => { Console.WriteLine("妇亦起大呼：哎呀，天哪，怎么着火了！"); };
+            southShow.FireHandlerEvent += () => { Console.WriteLine("两儿齐哭：哇啊~~，妈妈，妈妈"); };
+            southShow.FireHandlerEvent += () => { Console.WriteLine("百千人大呼：起火了，大家快来救火呀！"); };
+            southShow.FireHandlerEvent += () => { Console.WriteLine("百千儿哭：呜哇！呜哇！呜哇呜哇！"); };
+            southShow.FireHandlerEvent += () => { Console.WriteLine("百千犬吠：汪汪汪汪汪汪汪汪汪汪汪汪汪汪！"); };
+
+            northShow.FireHandlerEvent += () => { Console.WriteLine("夫起大户:老婆，着火了！！"); };
+            northShow.FireHandlerEvent += () => { Console.WriteLine("妇亦起大呼:哎呀妈呀，好吓人呀！！"); };
+
+            westShow.FireHandlerEvent += () => { Console.WriteLine("夫起大户:老婆快起来，又着火了！！"); };
+            westShow.FireHandlerEvent += () => { Console.WriteLine("妇亦起大呼:快抱着孩子跑呀！！"); };
+            westShow.FireHandlerEvent += () => { Console.WriteLine("两儿齐哭:妈妈，好怕！！"); };
+
+            eastShow.FireHandlerEvent += () => { Console.WriteLine("夫起大户:了不得了起大火了，老婆！！"); };
+            eastShow.FireHandlerEvent += () => { Console.WriteLine("妇亦起大呼:别傻叫了，快跑！！"); };
+
+            Console.ReadLine();
+
+        }
+
+        private static void SouthShow_TemperChangeHandlerEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void play<T>(T show)
+            where T:BaseShow,ICharge
+        {
+            show.Start();
+            show.OpenRemark();
+            show.DogBark();
+            show.PeopelTalk();
+            show.WindBlow();
+            show.EndRemark();
+            show.charge();
+            Console.WriteLine("");
+        }
+        public static void JueHuo(Action action)
+        {
+            Console.WriteLine("绝活马上开始了。。。");
+            action.Invoke();
+            Console.WriteLine("绝活表演结束，大家鼓掌！！");
+            Console.WriteLine(shortSpliter);
+        }
+        public static void ShowDetail<T>(T show)
+            where T : BaseShow
+        {
+
+            Type t = typeof(T);
+            PropertyInfo[] memberInfos = t.GetProperties( );
+            Console.WriteLine($"{t.Name}");
+            string txt = "  属性：  ";
+            foreach (PropertyInfo memberInfo in memberInfos)
+                txt += $"{memberInfo.Name}:{memberInfo.GetValue(show)};    ";
+            Console.WriteLine(txt.TrimEnd());
+            FieldInfo[] fielfInfos = t.GetFields();
+            
+            txt = "  字段：  ";
+            foreach (FieldInfo info in fielfInfos)
+            { 
+
+                txt += $"{info.Name}:{info.GetValue(show)};    ";
+            }
+            Console.WriteLine(txt);
+        //    Console.WriteLine(spliter);
         }
     }
 }
